96ad8f0d7b16c9113aa1bf3ccf9a76e691b2c779
braydenmackay <braydenmackay1@gmail.com>
2020-05-01 19:50:26 -0600

    initial commit

---
+++ screens/FeedScreen.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { Text, View, TouchableOpacity } from "react-native"
+
+interface IFeedScreenProps {
+  navigation: {
+    navigate: (arg: string) => void
+  }
+}
+export default (props: IFeedScreenProps) => {
+  return (
+    <View>
+      <Text>Feed Screen</Text>
+
+      <TouchableOpacity onPress={() => props.navigation.navigate("Search")}>
+        <Text>Search</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
---
+++ utils/router.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { createAppContainer, createSwitchNavigator } from "react-navigation"
+import { createStackNavigator } from "react-navigation-stack"
+
+import FeedScreen from "../screens/FeedScreen"
+import SearchScreen from "../screens/SearchScreen"
+import AccountScreen from "../screens/AccountScreen"
+import PostFormScreen from "../screens/PostFormScreen"
+
+const AppStack = createStackNavigator(
+  {
+    Feed: FeedScreen,
+    Search: SearchScreen,
+    Account: AccountScreen,
+    PostForm: PostFormScreen
+  },
+  {
+    initialRouteName: "Feed"
+  }
+)
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      App: AppStack
+    },
+    {
+      initialRouteName: "App"
+    }
+  )
+)
